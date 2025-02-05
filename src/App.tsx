149fa57529daec18e387df4583f5bfddb9f64630
jannik_riegel <jannik.riegel@gmail.com>
2025-02-05 09:49:55 +0100

    added profile page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./index.css";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landingPage/LandingPage";
 import TestPage from "./pages/testPage/TestPage";
-
+import ProfilePage from "./pages/profilePage/ProfilePage";
 
 // import { AuthProvider } from "./contexts/AuthContext";
 
@@ -12,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/test" element={<TestPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
     </Routes>
     // </AuthProvider>
   );
